b0a488042d68a867ca9f8c153fd2952883d7fb62
joaore10 <joaore1000@gmail.com>
2024-04-20 12:03:48 -0300

    corrigindo o editar dopoimento

--- admin/php/apagarImagem.php
+++ admin/php/apagarImagem.php
@@ -4,15 +4,16 @@ include("../php/sessao.php");
 session_write_close();
 include("../includes/db.php");
 
-$id = $_GET['id'];
-$pasta = $_GET['pasta'];
-$pag = $_GET['pag'];
+$id = $_GET['id'];//noticias
+$pasta = $_GET['pasta'];//65
+$pag = $_GET['pag'];//40
 
 switch ($pasta){	
     case "categorias": 
             $tab="categorias_subcat";
             break;
-    default: $tab=$pasta;
+    default: 
+            $tab=$pasta;
             break;
 }
 
@@ -21,15 +22,15 @@ $dados = query($sql);
 
 
 $sql = "UPDATE $tab SET imagem='' WHERE id=$id ";
-	$conn = conecta();
-	$q = @mysqli_query($conn, $sql);	
-        @((is_null($___mysqli_res = mysqli_close($conn))) ? false : $___mysqli_res);
+$conn = conecta();
+$q = @mysqli_query($conn, $sql);	
+@((is_null($___mysqli_res = mysqli_close($conn))) ? false : $___mysqli_res);
         
-$caminho = "../imagens/".$pasta."/".$dados[0][imagem];
+$caminho = "../imagens/".$pasta."/".$dados[0]['imagem'];
 if (file_exists($caminho)){	
             unlink ($caminho);
 }
-$caminho = "../imagens/".$pasta."/thumb_".$dados[0][imagem];
+$caminho = "../imagens/".$pasta."/thumb_".$dados[0]['imagem'];
 if (file_exists($caminho))
 {
 	unlink ($caminho);
